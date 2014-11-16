b88198af375bf5ea81ebd33e859fe6d14e836624
Ionică Bizău <bizauionica@yahoo.com>
2014-11-16 12:28:13 +0200

    JSDoc comments

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ var Request = require("request")
   , Exec = require("child_process").exec
   ;
 
+// Constants
 const VP = "http://graph.facebook.com/victor.ponta"
     , KI = "http://graph.facebook.com/klausiohannis"
     ;
 
+/**
+ * seq
+ * Fetches the number of likes.
+ *
+ * @name seq
+ * @function
+ * @param {Function} callback The callback function.
+ * @return {undefined}
+ */
 function seq(callback) {
     Request({
         url: VP,
@@ -17,20 +27,29 @@ function seq(callback) {
             url: KI,
             json: true
         }, function (err, res, ki) {
-            debugger;
             callback(null, [vp.likes, ki.likes]);
         });
     });
 }
 
+/**
+ * show
+ * Executes `notiy-send` to show system notifications.
+ *
+ * @name show
+ * @function
+ * @param {Array} arr The array with the number of likes.
+ * @return {undefined}
+ */
 function show(arr) {
     Exec("notify-send -t 1000 'Victor Ponta: " + arr[0] + "\nKlauss Iohannis: " + arr[1] + " :: Diff: " + (arr[1] - arr[0]) + "'");
 }
 
+
+// Start
 seq(function (err, arr) {
     show(arr);
 });
-
 setInterval(function () {
     seq(function (err, arr) {
         show(arr);
